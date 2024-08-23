8161c13674c7a1c9edae7103eead0662e97425ee
ihebjemmali <ihebjemmali000@gmail.com>
2024-08-23 09:43:24 +0100

    finished

--- main.js
+++ main.js
@@ -241,7 +241,20 @@ function showCollection(collectionName) {
   displayItems(collection, "itemsContainer");
 }
 
+function addItem() {
+  var itemName = document.getElementById("addItemName").value;
+  addItemToCollection(stockCollection, itemName);
+  displayItems(stockCollection, "itemsContainer");
+}
 
+function addItemToCollection(collection, itemName) {
+  for (var i = 0; i < stockCollection.length; i++) {
+    if (stockCollection[i].name === itemName) {
+      collection.push(stockCollection[i]);
+      return;
+    }
+  }
+}
 
 function removeItem() {
   var itemName = document.getElementById("removeItemName").value;
@@ -276,3 +289,33 @@ function editItemInCollection(collection, itemName, newPrice, newQuantity) {
   }
 }
 
+function searchItems() {
+  var search = document.getElementById("search").value;
+  var result = searchItem(mensCollection, search);
+  displayItems(result, "itemsContainer");
+}
+
+function searchItem(collection, search) {
+  return collection.filter(function (item) {
+    return item.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
+}
+
+function addToFavorites() {
+  var itemName = document.getElementById("favoriteItemName").value;
+  for (var i = 0; i < mensCollection.length; i++) {
+    if (mensCollection[i].name === itemName) {
+      favoriteCollection.push(mensCollection[i]);
+      return;
+    }
+  }
+
+  for (var j = 0; j < womensCollection.length; j++) {
+    if (womensCollection[j].name === itemName) {
+      favoriteCollection.push(womensCollection[j]);
+      return;
+    }
+  }
+
+  displayItems(favoriteCollection, "favoritesContainer");
+}
